5d36f6424ff0a87e6885f23c4c1d8ddd8c061fcf
Anda Stefania Ghita <104030121+AndaGhita@users.noreply.github.com>
2023-02-09 11:15:03 +0100

    Update power.py

--- Lectures/Lecture 6/power.py
+++ Lectures/Lecture 6/power.py
@@ -4,3 +4,13 @@
 # power(4,0) -> returnera 1
 
 # power(4,n=2) -> returnera 4*power(4,n-1)
+
+import math
+
+def power_funktion(x,n):
+    if x == 0:
+        return 0
+    else:
+        return pow(x,n)
+
+print(power_funktion(4,6))
